27fb70eba476570fb2489ac872126c158c241b30
rivkastorch <98669403+rivkastorch@users.noreply.github.com>
2022-05-31 18:08:10 -0400

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<?php
+echo
+'<div class="sidenav">
+    <a href="about.php">About</a>
+    <a href="editorChoice.php">Recipes from Editor</a>
+    <a href="Calendar.html">Calendar</a>
+    <a href="userRecipes.php">All Other Recipes</a>
+    <a href="hebcal/examples.html">conversion</a>
+    <a href="RoshHashana.html">Rosh Hashana</a>
+    <a href="Sukkos.html">Sukkos</a>
+    <a href="Chanukah.html">Chanuka</a>
+    <a href="Purim.html">Purim</a>
+    <a href="Pesach.html">Pesach</a>
+    <a href="Shavuos.html">Shavuos</a>
+ </div>';
+?>
