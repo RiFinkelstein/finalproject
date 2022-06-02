b9f99c200e1546c5215ff4618473fa56bdb296f1
rivkastorch <98669403+rivkastorch@users.noreply.github.com>
2022-06-02 00:38:21 -0400

    Update userRecipes.php

--- userRecipes.php
+++ userRecipes.php
@@ -10,8 +10,20 @@
 <body>
     <div class="main">
         <h2>Recipes Page</h2>
- 
-    <p><button onclick="location.href='addRecipe.php'">Add Recipe</button></p>
+        <?php
+    include 'dbConnect.php';
+    $sql = "SELECT * FROM `recipeTable`;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+            echo '<span style="font-size: 40px; color: #000080; font-weight: bold;">', $row['title']."<br>".'</span>';
+            echo '<span style="font-size: 30px; color: #FFFFFF; font-weight: normal;">', $row['ingredients']."<br>". '</span>';
+            echo '<span style="font-size: 30px; color: #000000; font-weight: medium;">', $row['instructions']."<br>". "<br>".'</span>';
+        }
+    }
+?>
+    <p><button style="width:auto" onclick="location.href='addRecipe.php'">Add Recipe</button></p>
     <p><button onclick="location.href='main.php'">Main</button></p> 
     </div>
 </body>
